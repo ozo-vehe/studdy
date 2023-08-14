312bb6cf2c0e2c069896464d55d5e4bf064f6a3c
ozo-vehe <johnnie.vehe@gmail.com>
2023-08-15 00:52:19 +0100

    added padding to the service section

--- src/components/ServiceSection.vue
+++ src/components/ServiceSection.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <main class="pt-20 pb-12 min-h-screen flex flex-wrap gap-x-16 gap-y-4 items-start justify-center">
+  <main class="px-12 pt-20 pb-12 min-h-screen flex flex-wrap gap-x-16 gap-y-4 items-start justify-center">
     <section class="serviceImage w-400 h-450">
       <img class="w-full h-full object-contain" src="../assets/images/service.png" alt="service" />
     </section>
@@ -18,54 +18,50 @@
 
       <section class="services mt-8 flex flex-wrap gap-y-4 items-start justify-between">
         
-        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-2">
-          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5">
+        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-4">
+          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5 mb-2">
             <img class="w-full h-full object-contain" src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/96/ffffff/external-forum-content-creator-tanah-basah-basic-outline-tanah-basah.png" alt="Group"/>
           </div>
           <h3 class="font-bold mb-2">Study Group Formation</h3>
           <p class="text-sm">
             Connect with fellow students who share your academic 
             interests and goals, and collaborate effectively to achieve 
-            better results. Join or create study groups centered around 
-            your chosen subjects or courses.
+            better results.
           </p>
         </div>
 
-        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-2">
-          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5">
+        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-4">
+          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5 mb-2">
             <img class="w-full h-full object-contain" src="https://img.icons8.com/ios/50/ffffff/administrative-tools.png" alt="Preference"/>
           </div>
           <h3 class="font-bold mb-2">Customizable Preferences</h3>
           <p class="text-sm">
-            Tailor your study experience by indicating your preferred 
-            study methods. Find study partners who match your learning 
-            style, whether you prefer group discussions, virtual study 
+            Find study partners who match your learning 
+            style, be it group discussions, virtual study 
             sessions, or independent learning.
           </p>
         </div>
 
-        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-2">
-          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5">
+        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-4">
+          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5 mb-2">
             <img class="w-full h-full object-contain" src="https://img.icons8.com/pulsar-line/48/ffffff/contract-job.png" alt="Friendly profile"/>
           </div>
           <h3 class="font-bold mb-2">User-friendly Profiles</h3>
           <p class="text-sm">
             Create a personalized profile showcasing your academic 
-            background, interests, and study preferences. This helps 
-            you connect with like-minded peers and form study partnerships.
+            background, interests, and study preferences to help connect with like-minded peers.
           </p>
         </div>
 
-        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-2">
-          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5">
+        <div class="service border border-slate-100 rounded-md w-60 shadow-sm py-4 px-4">
+          <div class="bg-custom-dark-green rounded-full w-10 h-10 p-1.5 mb-2">
             <img class="w-full h-full object-contain" src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/96/ffffff/external-file-sharing-data-science-and-cyber-security-flatart-icons-outline-flatarticons.png" alt="Sharing"/>
           </div>
           <h3 class="font-bold mb-2">Resource Sharing</h3>
           <p class="text-sm">
-            Access a vast library of study materials, lecture notes, 
-            and reference materials shared by fellow students. 
-            Contribute your own resources to help others succeed in 
-            their studies.
+            Access a vast library of study materials and lecture notes, 
+            shared by fellow students. Contribute your own resources 
+            to help others succeed.
           </p>
         </div>
       </section>
